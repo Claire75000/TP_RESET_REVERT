b15b5b1fe2f119c0c8f5eaa32a9cc9735c770866
claire <claire.barrault@assurance-maladie.fr>
2023-05-26 12:25:18 +0200

    commit 3 ko

--- module1/file1.py
+++ module1/file1.py
@@ -1,4 +1,4 @@
 def function1():
-    print("Function 1 from module 1 commit 2 ko ")
+    print("Function 1 from module 1 commit 3 ko ")
     print(some_undefined_variable)  # Cette ligne introduit une régression
     
